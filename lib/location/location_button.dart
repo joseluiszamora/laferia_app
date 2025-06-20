0197b34704b6d8e53915103d1bfced1a3d915350
jzamora <jzamora@miteleferico.bo>
2025-06-20 13:46:39 -0400

    mejorar el zoom center controls

--- lib/location/location_button.dart
+++ lib/location/location_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../core/blocs/location/location.dart';
+import '../core/blocs/location/location.dart';
 
 class LocationButton extends StatelessWidget {
   final VoidCallback? onLocationFound;
